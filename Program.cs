e35e5f4bd649611d7908ea044944157eee11ff6e
Daniel Molina Yepes <programanime@gmail.com>
2022-07-07 22:02:02 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i am there!");
+        }
+    }
+} 
